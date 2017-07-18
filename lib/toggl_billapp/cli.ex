2869bc51d2749cd0d50734379143bf95c03bf2c8
Jakub Kottnauer <jk@jakubkottnauer.com>
2017-07-18 14:01:17 +0200

    Add first API fetch

--- lib/toggl_billapp/cli.ex
+++ lib/toggl_billapp/cli.ex
@@ -1,6 +1,10 @@
 defmodule TogglBillapp.CLI do
   def main(args) do
     togglApiKey = Application.get_env(:toggl_billapp, :togglApiKey)
-    client = Togglex.Client.new(%{access_token: togglApiKey}, :api)
+    togglWorkspaceId = Application.get_env(:toggl_billapp, :togglWorkspaceId)
+    togglUserId = Application.get_env(:toggl_billapp, :togglUserId)
+    client = Togglex.Client.new(%{access_token: togglApiKey}, :reports)
+    x = Togglex.Reports.summary(client, %{workspace_id: togglWorkspaceId, user_ids: togglUserId, grouping: "projects", subgrouping: "time_entries", since: "2017-07-01", until: "2017-07-31"})
+    IO.inspect x
   end
 end
