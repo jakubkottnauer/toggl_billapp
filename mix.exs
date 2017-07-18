284a8a8b885b929da6736d6491f16d34d1465c2a
Jakub Kottnauer <jk@jakubkottnauer.com>
2017-07-18 13:11:21 +0200

    Add excoveralls

--- mix.exs
+++ mix.exs
@@ -2,34 +2,28 @@ defmodule TogglBillapp.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :toggl_billapp,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     escript: [main_module: TogglBillapp.CLI],
-     deps: deps()]
+    [
+      app: :toggl_billapp,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      escript: [main_module: TogglBillapp.CLI],
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [applications: [:togglex]]
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:togglex, "~> 0.1.0"}]
+    [
+      {:togglex, "~> 0.1.0"},
+      {:excoveralls, "~> 0.7", only: :test}
+    ]
   end
 end
